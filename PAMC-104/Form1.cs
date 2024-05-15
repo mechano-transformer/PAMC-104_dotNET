7f1e71f300c0e0d92525b04feb128e791f1b154f
tabularasa066a <tabularasa066a@gmail.com>
2024-05-15 23:55:58 +0900

    プロジェクト ファイルを追加します。

---
+++ PAMC-104/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAMC_104
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RS232C rs232c = new RS232C(serialPort);
+        }
+    }
+}
---
+++ PAMC-104/RS232C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAMC_104
+{
+    public class RS232C
+    {
+        string[] ports;
+
+        public RS232C(SerialPort serialPort)
+        {
+            ports = SerialPort.GetPortNames();
+            serialPort.PortName = ports[0];
+            serialPort.PortName = ports[0];
+            serialPort.BaudRate = 9600;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = System.IO.Ports.StopBits.One;
+            serialPort.Parity = System.IO.Ports.Parity.None;
+        }
+    }
+}
