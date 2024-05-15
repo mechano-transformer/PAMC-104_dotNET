74092772fa5df70629fb95e5732eb50f1a71bbf7
tabularasa066a <tabularasa066a@gmail.com>
2024-05-16 00:22:11 +0900

    メッセージ送受信メソッドを実装

--- PAMC-104/RS232C.cs
+++ PAMC-104/RS232C.cs
@@ -2,24 +2,89 @@
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PAMC_104
 {
     public class RS232C
     {
         string[] ports;
+        SerialPort _serialPort;
+        string DELIMITER = "\r\n";
 
         public RS232C(SerialPort serialPort)
         {
+            // ポート情報を初期化
             ports = SerialPort.GetPortNames();
-            serialPort.PortName = ports[0];
-            serialPort.PortName = ports[0];
-            serialPort.BaudRate = 9600;
-            serialPort.DataBits = 8;
-            serialPort.StopBits = System.IO.Ports.StopBits.One;
-            serialPort.Parity = System.IO.Ports.Parity.None;
+            _serialPort = serialPort;
+            _serialPort.PortName = ports[0];
+            _serialPort.PortName = ports[0];
+            _serialPort.BaudRate = 9600;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Parity = Parity.None;
+
+            // ポートオープン
+            open();
+        }
+
+        // ポートオープン
+        private void open()
+        {
+            // 既にポートが開いてるときは何もしない
+            if (_serialPort.IsOpen) return;
+
+            try
+            {
+                _serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        // ポートを閉じる
+        private void close()
+        {
+            if (_serialPort.IsOpen == true) _serialPort.Close();
+        }
+
+        // RS232C経由で情報を送信
+        public void send(string content)
+        {
+            _serialPort.NewLine = DELIMITER;
+            try
+            {
+                System.Console.WriteLine(content); // とりあえず動作検証用にコンソール出力
+                // _serialPort.Write(content); // 本チャンはこうやってRS232C経由で書きこむ
+            }
+            catch (Exception ex) 
+            {
+
+            }
+        }
+
+        // RS232C経由で情報を受信
+        public void receive(SerialDataReceivedEventArgs e)
+        {
+            string strDataReceived = "";
+            try
+            {
+                strDataReceived = _serialPort.ReadLine();
+                MessageBox.Show(strDataReceived);
+
+            }
+            catch (Exception ex)
+            {
+                // MessageBox.Show(ex.Message); 一応これダイアログ出るね
+            }
+
+            // ポートを閉じる
+            close();
         }
     }
 }
