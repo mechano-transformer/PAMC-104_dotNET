409e24dfd1ab7353e8212984f81c5d0e196be835
tabularasa066a <tabularasa066a@gmail.com>
2024-05-16 21:38:20 +0900

    フォームの入力値からコマンドを生成

--- PAMC-104/MainForm.Designer.cs
+++ PAMC-104/MainForm.Designer.cs
@@ -30,23 +30,154 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.plus_btn = new System.Windows.Forms.Button();
+            this.minus_btn = new System.Windows.Forms.Button();
+            this.frequency_form = new System.Windows.Forms.TextBox();
+            this.frequency_lbl = new System.Windows.Forms.Label();
+            this.numOfPulses_lbl = new System.Windows.Forms.Label();
+            this.numOfPulses_form = new System.Windows.Forms.TextBox();
+            this.cmdToSend_lbl = new System.Windows.Forms.Label();
+            this.cmdDirection_text = new System.Windows.Forms.TextBox();
+            this.cmdFrequency_text = new System.Windows.Forms.TextBox();
+            this.cmdNumOfPulses_text = new System.Windows.Forms.TextBox();
+            this.cmdPort_text = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // plus_btn
+            // 
+            this.plus_btn.Location = new System.Drawing.Point(335, 81);
+            this.plus_btn.Name = "plus_btn";
+            this.plus_btn.Size = new System.Drawing.Size(31, 34);
+            this.plus_btn.TabIndex = 0;
+            this.plus_btn.Text = "+";
+            this.plus_btn.UseVisualStyleBackColor = true;
+            this.plus_btn.Click += new System.EventHandler(this.plus_btn_Click);
+            // 
+            // minus_btn
+            // 
+            this.minus_btn.Location = new System.Drawing.Point(433, 81);
+            this.minus_btn.Name = "minus_btn";
+            this.minus_btn.Size = new System.Drawing.Size(32, 34);
+            this.minus_btn.TabIndex = 1;
+            this.minus_btn.Text = "-";
+            this.minus_btn.UseVisualStyleBackColor = true;
+            this.minus_btn.Click += new System.EventHandler(this.minus_btn_Click);
+            // 
+            // frequency_form
+            // 
+            this.frequency_form.Location = new System.Drawing.Point(335, 160);
+            this.frequency_form.Name = "frequency_form";
+            this.frequency_form.Size = new System.Drawing.Size(100, 19);
+            this.frequency_form.TabIndex = 2;
+            this.frequency_form.TextChanged += new System.EventHandler(this.frequency_form_TextChanged);
+            // 
+            // frequency_lbl
+            // 
+            this.frequency_lbl.AutoSize = true;
+            this.frequency_lbl.Location = new System.Drawing.Point(335, 142);
+            this.frequency_lbl.Name = "frequency_lbl";
+            this.frequency_lbl.Size = new System.Drawing.Size(55, 12);
+            this.frequency_lbl.TabIndex = 3;
+            this.frequency_lbl.Text = "frequency";
+            // 
+            // numOfPulses_lbl
+            // 
+            this.numOfPulses_lbl.AutoSize = true;
+            this.numOfPulses_lbl.Location = new System.Drawing.Point(337, 213);
+            this.numOfPulses_lbl.Name = "numOfPulses_lbl";
+            this.numOfPulses_lbl.Size = new System.Drawing.Size(62, 12);
+            this.numOfPulses_lbl.TabIndex = 4;
+            this.numOfPulses_lbl.Text = "# of pulses";
+            // 
+            // numOfPulses_form
+            // 
+            this.numOfPulses_form.Location = new System.Drawing.Point(337, 229);
+            this.numOfPulses_form.Name = "numOfPulses_form";
+            this.numOfPulses_form.Size = new System.Drawing.Size(100, 19);
+            this.numOfPulses_form.TabIndex = 5;
+            this.numOfPulses_form.TextChanged += new System.EventHandler(this.numOfPulses_form_TextChanged);
+            // 
+            // cmdToSend_lbl
+            // 
+            this.cmdToSend_lbl.AutoSize = true;
+            this.cmdToSend_lbl.Location = new System.Drawing.Point(335, 274);
+            this.cmdToSend_lbl.Name = "cmdToSend_lbl";
+            this.cmdToSend_lbl.Size = new System.Drawing.Size(96, 17);
+            this.cmdToSend_lbl.TabIndex = 6;
+            this.cmdToSend_lbl.Text = "command to send";
+            this.cmdToSend_lbl.UseCompatibleTextRendering = true;
+            // 
+            // cmdDirection_text
+            // 
+            this.cmdDirection_text.Location = new System.Drawing.Point(337, 295);
+            this.cmdDirection_text.Name = "cmdDirection_text";
+            this.cmdDirection_text.ReadOnly = true;
+            this.cmdDirection_text.Size = new System.Drawing.Size(29, 19);
+            this.cmdDirection_text.TabIndex = 7;
+            // 
+            // cmdFrequency_text
+            // 
+            this.cmdFrequency_text.Location = new System.Drawing.Point(370, 295);
+            this.cmdFrequency_text.Name = "cmdFrequency_text";
+            this.cmdFrequency_text.ReadOnly = true;
+            this.cmdFrequency_text.Size = new System.Drawing.Size(45, 19);
+            this.cmdFrequency_text.TabIndex = 8;
+            // 
+            // cmdNumOfPulses_text
+            // 
+            this.cmdNumOfPulses_text.Location = new System.Drawing.Point(418, 295);
+            this.cmdNumOfPulses_text.Name = "cmdNumOfPulses_text";
+            this.cmdNumOfPulses_text.ReadOnly = true;
+            this.cmdNumOfPulses_text.Size = new System.Drawing.Size(47, 19);
+            this.cmdNumOfPulses_text.TabIndex = 9;
+            // 
+            // cmdPort_text
+            // 
+            this.cmdPort_text.Location = new System.Drawing.Point(467, 295);
+            this.cmdPort_text.Name = "cmdPort_text";
+            this.cmdPort_text.ReadOnly = true;
+            this.cmdPort_text.Size = new System.Drawing.Size(29, 19);
+            this.cmdPort_text.TabIndex = 10;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(659, 377);
+            this.Controls.Add(this.cmdPort_text);
+            this.Controls.Add(this.cmdNumOfPulses_text);
+            this.Controls.Add(this.cmdFrequency_text);
+            this.Controls.Add(this.cmdDirection_text);
+            this.Controls.Add(this.cmdToSend_lbl);
+            this.Controls.Add(this.numOfPulses_form);
+            this.Controls.Add(this.numOfPulses_lbl);
+            this.Controls.Add(this.frequency_lbl);
+            this.Controls.Add(this.frequency_form);
+            this.Controls.Add(this.minus_btn);
+            this.Controls.Add(this.plus_btn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button plus_btn;
+        private System.Windows.Forms.Button minus_btn;
+        private System.Windows.Forms.TextBox frequency_form;
+        private System.Windows.Forms.Label frequency_lbl;
+        private System.Windows.Forms.Label numOfPulses_lbl;
+        private System.Windows.Forms.TextBox numOfPulses_form;
+        private System.Windows.Forms.Label cmdToSend_lbl;
+        private System.Windows.Forms.TextBox cmdDirection_text;
+        private System.Windows.Forms.TextBox cmdFrequency_text;
+        private System.Windows.Forms.TextBox cmdNumOfPulses_text;
+        private System.Windows.Forms.TextBox cmdPort_text;
     }
 }
 
--- PAMC-104/MainForm.cs
+++ PAMC-104/MainForm.cs
@@ -13,6 +13,11 @@ namespace PAMC_104
 {
     public partial class MainForm : Form
     {
+        //private string command = "";
+        private string cmd_direction = "";
+        private string cmd_frequency = "";
+        private string cmd_numOfPulses = "";
+
         public MainForm()
         {
             InitializeComponent();
@@ -22,6 +27,34 @@ namespace PAMC_104
         {
             RS232C rs232c = new RS232C(serialPort);
             rs232c.send("NR15000010A");
+        }
+
+        private void plus_btn_Click(object sender, EventArgs e)
+        {
+            cmdDirection_text.Text = "NR";
+        }
+
+        private void minus_btn_Click(object sender, EventArgs e)
+        {
+            cmdDirection_text.Text = "RR";
+        }
+
+        private void frequency_form_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(frequency_form.Text, out int inputVal))
+            {
+                string result = inputVal.ToString("D4");
+                cmdFrequency_text.Text = result;
+            }
+        }
+
+        private void numOfPulses_form_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(numOfPulses_form.Text, out int inputVal))
+            {
+                string result = inputVal.ToString("D4");
+                numOfPulses_form.Text = result;
+            }
         }
     }
 }
